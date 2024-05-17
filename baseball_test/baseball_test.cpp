2175d5e05bf1d02543242e71d1803ce68b9a694c
jinwk.lee <jinwk.lee@samsung.com>
2024-05-17 13:56:11 +0900

    [feature] 28

--- baseball_test/baseball_test.cpp
+++ baseball_test/baseball_test.cpp
@@ -1,8 +1,7 @@
 #include "pch.h"
 #include "../basball/basball.cpp"
 
-TEST(BaseballGame, TryGameTest) {
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
-	EXPECT_EQ(1, 1);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
