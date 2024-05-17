6b0ac88ad70f332b553e684ae9090ee37765d82f
jinwk.lee <jinwk.lee@samsung.com>
2024-05-17 14:15:02 +0900

    [refactor] 41

--- basball/basball.cpp
+++ basball/basball.cpp
@@ -4,19 +4,30 @@ using namespace std;
 class Baseball {
 public:
 	void guess(const string& guessNumber) {
+		assertIllegalArgument(guessNumber);
+	}
+
+	bool isDuplicatedNumber(const string& guessNumber) {
+		if (guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2]) {
+			return true;
+		}
+		return false;
+	}
+
+	void assertIllegalArgument(const string& guessNumber) {
 		if (guessNumber.length() != 3) {
 			throw length_error("Must be three letters.");
 		}
 
 		for (char ch : guessNumber) {
-			if (ch < '0' || ch > '9') {
+			if (ch >= '0' && ch <= '9') {
 				throw invalid_argument("Must be number.");
 			}
 		}
 
-		if (guessNumber[0] == guessNumber[1]
-			|| guessNumber[0] == guessNumber[2]
-			|| guessNumber[1] == guessNumber[2]) {
+		if (isDuplicatedNumber(guessNumber)) {
 			throw invalid_argument("Must not have the same number.");
 		}
 	}
